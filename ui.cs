7095be262a64d1d722f5d5364e486baff8a42c9f
James28751 <117889870+James28751@users.noreply.github.com>
2022-11-18 21:49:38 +0800

    第二修改

--- ui.cs
+++ ui.cs
@@ -9,6 +9,7 @@ public class ui : MonoBehaviour
      GameObject car;
      GameObject UItext;
     GameObject over;
+    GameObject MDF;
 
 
 
@@ -19,22 +20,23 @@ public class ui : MonoBehaviour
         flag = GameObject.Find("flag");
        UItext = GameObject.Find("UItext");
         over = GameObject.Find("overtext");
+        MDF= GameObject.Find("MDF");
     }
 
     // Update is called once per frame
     void Update()
     {
-        double er = flag.transform.position.x -
+        float er = flag.transform.position.x -
             car.transform.position.x;
         UItext.GetComponent<Text>().text = "距離目標還有 " + er.ToString("F2") + "m";
-        
         if (Input.GetMouseButtonUp(0))
         {
            
             if (er > 0)
             {
-                double eq = car.transform.position.x;
-                double en = eq * 4;
+                float eq = (car.transform.position.x - MDF.transform.position.x) / (flag.transform.position.x-MDF.transform.position.x);
+                //車的位置-出發點(-1)/旗幟位置-出發點(25-(-1)) 11/26 %數
+                float en = eq * 100;
                 over.GetComponent<Text>().text = "分數: " + en.ToString("F2") + "分";
             }
             else if (er < 0)
